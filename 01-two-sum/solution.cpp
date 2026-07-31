7ced6320c78d5a08888b47b8a2ba41e6b237a279
Souvik Mondal <souvkmndal@gmail.com>
2026-07-31 10:40:56 +0530

    Update solution: Two Sum

--- 01-two-sum/solution.cpp
+++ 01-two-sum/solution.cpp
@@ -5,8 +5,8 @@ public:
         for( int i=0 ; i<nums.size() ; i++ ){
             int rem = target-nums[i];
             if( mp.find(rem)!= mp.end()){
-                // return {i,mp[rem]};
-                return nums[100];
+                return {i,mp[rem]};
+                // return nums[100];
             }
             if( mp.find(rem)== mp.end()){
                 mp[nums[i]]=i;
