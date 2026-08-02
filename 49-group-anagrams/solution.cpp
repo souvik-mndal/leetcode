aa7224aa90095717bd7d703e00ea3f0299ee9121
Souvik Mondal <souvkmndal@gmail.com>
2026-08-02 12:04:28 +0530

    Add solution: Group Anagrams

---
+++ 49-group-anagrams/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>>mp;
+        for( auto &it:strs ){
+            string temp = it;
+            sort(temp.begin() , temp.end());
+            mp[temp].push_back(it);
+        }
+        vector<vector<string>>str;
+        for( auto &it:mp ){
+            str.push_back(it.second);
+        }
+        return str;
+    }
+};
