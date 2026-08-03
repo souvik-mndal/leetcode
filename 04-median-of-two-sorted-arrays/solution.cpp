2041f454cf839bd105a4c8aaf2b384a274661138
Souvik Mondal <souvkmndal@gmail.com>
2026-08-03 13:34:57 +0530

    Update solution: Median of Two Sorted Arrays

--- 04-median-of-two-sorted-arrays/solution.cpp
+++ 04-median-of-two-sorted-arrays/solution.cpp
@@ -1,71 +1,35 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int i=0 , j=0;
-        int n=nums1.size();
-        int m=nums2.size();
-        int cnt  = 0;
-        int val1=0,val2=0;
-        int t=n+m;
-        int find1=0,find2=0;
-        if( t%2 == 1 ){
-            find2 = t/2;
-            find1=-1;
-        }
-        else{
-            find2 = t/2;
-            find1 = t/2-1;
-        }
-        while( i<n && j<m ){
-            if( nums1[i]<=nums2[j] ){
-                if( cnt == find1 ){
-                    val1 = nums1[i];
-                }
-                if( cnt == find2 ){
-                    val2 = nums1[i];
-                }
-                cnt++;
-                i++;
+    string check( string s , int st , int end ){
+        string store;
+        while( st>=0 && end<s.size() ){
+            if( s[st] == s[end] ){
+                store = s.substr(st,(end-st)+1);
             }
             else{
-                if( cnt == find1 ){
-                    val1 = nums2[j];
-                }
-                if( cnt == find2 ){
-                    val2 = nums2[j];
-                }
-                cnt++;
-                j++;
+                break;
+            }
+            st--;
+            end++;
+        }
+        return store;
+    }
+    string longestPalindrome(string s) {
+        string ans = "";
+        for( int i=0 ; i<s.size() ; i++ ){
+            string one = check(s,i,i);
+            string two = check(s,i,i+1);
+            string temp;
+            if( one.size() > two.size() ){
+                temp = one;
+            }
+            else{
+                temp = two;
+            }
+            if( temp.size() > ans.size() ){
+                ans = temp;
             }
         }
-        while( i<n  ){
-            if( cnt == find1 ){
-                    val1 = nums1[i];
-                }
-                if( cnt == find2 ){
-                    val2 = nums1[i];
-                }
-                cnt++;
-                i++;
-            
-        }
-        while(  j<m ){
-                if( cnt == find1 ){
-                    val1 = nums2[j];
-                }
-                if( cnt == find2 ){
-                    val2 = nums2[j];
-                }
-                cnt++;
-                j++;
-        }
-            // return (double)(val1+val2)/2.0;
-        if( find1 == -1 ){
-            return (float)val2;
-        }
-        else{
-            return (val1+val2)/2.0;        
-
-        }
+        return ans;
     }
 };
