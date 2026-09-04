149de391e303a3a0082bcbfeeae40f4224231497
Souvik Mondal <souvkmndal@gmail.com>
2026-09-04 18:38:37 +0530

    Add solution: Missing Number (Cpp)

---
+++ 268-missing-number/Cpp/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>hash(n+1,0);
+        for( int i=0 ; i<n ; i++ ){
+            hash[nums[i]]++;
+        }
+        for( int i=0 ; i<hash.size() ; i++ ){
+            if( hash[i] == 0 ){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
