a80ea69b7e667b2822a1c8176336bac0bdc2b0a7
Souvik Mondal <souvkmndal@gmail.com>
2026-09-05 22:29:08 +0530

    Add solution: Two Sum (Java)

---
+++ 01-two-sum/Java/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int remain = target - nums[i];
+
+            if (map.containsKey(remain)) {
+                return new int[] {i, map.get(remain)};
+            }
+
+            map.put(nums[i], i);
+        }
+        //return the value
+        return new int[] {};
+    }
+}
